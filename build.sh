17ec42787a05df9ba6e3f0bf36f7da2fec57f1df
Srivatsa Katta <vatsa.katta@gmail.com>
2017-08-18 21:40:43 -0400

    Build script fixed

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ set -x
 
 mvn clean install -DskipTests
 
-#docker build -t vatsakatta/vault-controller:latest ./cobalt-vault-controller/
-#docker push vatsakatta/vault-controller:latest
-#
-#docker build -t vatsakatta/token-retriever:latest ./cobalt-vault-token-retriever/
-#docker push vatsakatta/token-retriever:latest
+docker build -t vatsakatta/vault-controller:latest ./cobalt-vault-controller/
+docker push vatsakatta/vault-controller:latest
+
+docker build -t vatsakatta/token-retriever:latest ./cobalt-vault-token-retriever/
+docker push vatsakatta/token-retriever:latest
 
 docker build -t vatsakatta/vault-springapp:latest ./cobalt-vault-spring-app/
 docker push vatsakatta/vault-springapp:latest
